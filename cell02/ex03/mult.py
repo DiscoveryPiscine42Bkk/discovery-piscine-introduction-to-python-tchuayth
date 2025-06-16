cb6544e2bd386c611937c80f87a5ab5cc66cad42
tchuayth <tunwa05suki@gmail.com>
2025-06-16 23:53:47 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,13 @@
+num1 = int(input("Enter the frist number: "))
+num2 = int(input("Enter the second number: "))
 
+result = num1 * num2
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
+    
+print("Result of multiplication:", result)
