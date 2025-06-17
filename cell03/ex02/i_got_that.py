112485a832d24f41f2917a11f8661abe248f0b89
tchuayth <tunwa05suki@gmail.com>
2025-06-17 17:16:59 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,1 +1,7 @@
-
+while True:
+    user_input = input("Enter something (type 'STOP' to quit): ")
+    if user_input == "STOP":
+        print("Program stopped.")
+        break
+    else:
+        print("You said:", user_input)
