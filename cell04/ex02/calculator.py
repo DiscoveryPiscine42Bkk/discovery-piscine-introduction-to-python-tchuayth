4771dd899537924bf38e4cf81363272650c3c04e
tchuayth <tunwa05suki@gmail.com>
2025-06-17 18:04:09 +0700

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,1 +1,13 @@
+num1 = float(input("Give me the first number: "))
+num2 = float(input("Give me the second number: "))
 
+print("Got it!")
+print(f"{num1:g} + {num2:g} = {num1 + num2:g}")
+print(f"{num1:g} - {num2:g} = {num1 - num2:g}")
+
+if num2 != 0:
+    print(f"{num1:g} / {num2:g} = {num1 / num2:g}")
+else:
+    print(f"{num1:g} / {num2:g} = Cannot divide by zero")
+
+print(f"{num1:g} * {num2:g} = {num1 * num2:g}")
