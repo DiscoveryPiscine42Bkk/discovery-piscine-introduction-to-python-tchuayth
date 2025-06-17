c83c50066da473b38edc97abf8826a9f4fae97f7
tchuayth <tunwa05suki@gmail.com>
2025-06-17 18:15:49 +0700

    Update round_up.py

--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,1 +1,10 @@
+import math
 
+num_str = input("Enter a number: ")
+
+try:
+    num = float(num_str)
+    rounded_up = math.ceil(num)
+    print(f"The number rounded up is {rounded_up}")
+except ValueError:
+    print("Invalid input! Please enter a valid number.")
