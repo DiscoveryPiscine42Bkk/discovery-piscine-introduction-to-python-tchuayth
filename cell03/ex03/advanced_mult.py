ad19dbad39cc5f68f86fa611f8857ecf2a6f1ab6
tchuayth <tunwa05suki@gmail.com>
2025-06-17 17:34:43 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,1 +1,12 @@
+i = 1
+while i <= 12:
+    print(f"Multiplication table for {i}:")
+
+    j = 1
+    while j <= 12:
+        print(f"{i} x {j} = {i * j}")
+        j += 1
+
+    print()
+    i += 1
 
