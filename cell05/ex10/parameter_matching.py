0ec3735c3af7a59851c21eea8dcac310e6f5dc92
tchuayth <tunwa05suki@gmail.com>
2025-06-18 16:32:13 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,16 @@
+import sys
 
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+
+    param = sys.argv[1]
+    # รับ input จากผู้ใช้
+    answer = input("What was the parameter? ")
+
+    if answer == param:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+main()
