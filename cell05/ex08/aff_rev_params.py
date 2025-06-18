f843c039a368cd10df6a48d6fff7b301edadf901
tchuayth <tunwa05suki@gmail.com>
2025-06-18 16:29:15 +0700

    Update aff_rev_params.py

--- cell05/ex08/aff_rev_params.py
+++ cell05/ex08/aff_rev_params.py
@@ -1,1 +1,11 @@
+import sys
 
+def main():
+    params = sys.argv[1:]
+    if len(params) < 2:
+        print("none")
+    else:
+        # แสดงพารามิเตอร์ย้อนกลับทีละบรรทัด
+        for param in reversed(params):
+            print(param)
+main()
