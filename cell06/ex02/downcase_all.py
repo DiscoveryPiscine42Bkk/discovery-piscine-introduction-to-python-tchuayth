f03ea6065f635956bc394214e1c46714c54b4068
tchuayth <tunwa05suki@gmail.com>
2025-06-18 17:13:17 +0700

    Update downcase_all.py

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -1,1 +1,14 @@
+import sys
 
+def downcase_it(text):
+    return text.lower()
+
+def main():
+    params = sys.argv[1:]
+    if not params:
+        print("none")
+    else:
+        for p in params:
+            print(downcase_it(p))
+
+main()
