210d915f6af6e160c01eda2dbc6f7e2e89a1b71c
tchuayth <tunwa05suki@gmail.com>
2025-06-18 12:39:19 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,12 +1,14 @@
-i = 1
-while i <= 12:
-    print(f"Multiplication table for {i}:")
+import sys
 
-    j = 1
-    while j <= 12:
-        print(f"{i} x {j} = {i * j}")
-        j += 1
-
-    print()
-    i += 1
-
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i <= 10:
+        print(f"Table de {i}:", end=" ")
+        j = 0
+        while j <= 10:
+            print(i * j, end=" " if j < 10 else "")
+            j += 1
+        print()
+        i += 1
