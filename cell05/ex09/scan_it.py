5eda71751cc6619eef6117978faebd4567f05115
tchuayth <tunwa05suki@gmail.com>
2025-06-18 16:30:42 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,16 @@
+import sys
 
+def main():
+    if len(sys.argv) != 3:
+        print("none")
+        return
+
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+
+    count = text.count(keyword)
+    if count == 0:
+        print("none")
+    else:
+        print(count)
+main()
